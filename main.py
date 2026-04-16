fab5a79522c732ddc2bfc83a3f2b19304b5257a2
Andoryuc <l2.pipe2012@gmail.com>
2026-04-16 06:03:10 -0500

    ajuste tolerancia

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ if corte_actual == "PHASE 01: RAÍCES":
                 x0 = c1.number_input("X0", value=0.0)
                 x1 = c2.number_input("X1", value=1.0)
                 
-            tol = st.number_input("Tolerance", value=0.0001, format="%.6f")
+            tol = st.number_input("Tolerance", value=0.0000000000001, format="%.6f")
             
             if st.button("RUN CALCULATION"):
                 try:
