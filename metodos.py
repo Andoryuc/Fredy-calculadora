7ccfe746846ed3225658006c205b9856e66e3887
Andoryuc <l2.pipe2012@gmail.com>
2026-04-16 08:16:45 -0500

    float

--- metodos.py
+++ metodos.py
@@ -28,7 +28,7 @@ def biseccion(expresion, x, a, b, tolerancia):
   if f_a * f_b >= 0:
     return None, 0
 
-  iteraciones = int(np.ceil(np.log2((b - a) / tolerancia)))
+  iteraciones = float(np.ceil(np.log2((b - a) / tolerancia)))
   
   for i in range(iteraciones):
     c = (a + b) / 2
